4967ca688c4de9965707ff4894d6f613ce5374a8
govind <govind.g.coder@gmail.com>
2026-02-17 21:36:56 +0530

    initial commit

--- src/ThrowsDemo.java
+++ src/ThrowsDemo.java
@@ -3,17 +3,22 @@ import java.io.IOException;
 
 public class ThrowsDemo {
     
-    // TODO: Define static method check(int n) that throws IOException
+    // Define static method check(int n) that throws IOException
     // If n < 0, throw new IOException()
     // Else, print n
-
-   
-        // TODO: Read n
-        
-        // TODO: try {
-        //     call check(n)
-        // } catch (IOException e) {
-        //     Print "Caught: IO Exception"
-        // }
+    public static void check(int n) throws IOException{
+        if(n<0) throw new IOException();
+        else System.out.print(n);
+    }
     
+    public static void main(String[] args){
+        // Read n
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        try {
+            check(n);
+        } catch (IOException e) {
+            System.out.print("Caught: IO Exception");
+        }
+    }
 }
